ea35c40cc2e772bf9903f0923a4ddc8c931c6e28
m-kuzmin <71077087+m-kuzmin@users.noreply.github.com>
2025-04-01 12:39:36 +0000

    squash! try not splitting disko config into multiple files

--- disko/multi-user-workstation.nix
+++ disko/multi-user-workstation.nix
@@ -15,86 +15,84 @@ let
   There is a risk of the user password being compromised because it is also stored in /etc/shadow.
   However, this setup provides significant convinience over the slight security compromise.
   */
-  partitions = {
-    efiboot = {
-      size = "512M";
-      type = "EF00";
-      content = {
-        type = "filesystem";
-        format = "vfat";
-        mountpoint = "/boot";
-        mountOptions = ["umask=0077"];
-      };
-    };
-
-    # Nix store & impermanence old roots
-    system = {
-      type = "8303";
-      priority = "200";
-      size = "100G";
-      content = {
-        type = "luks";
-        name = "nixos-system";
-        settings = {
-          allowDiscards = true;
-        };
-        content = {
-          type = "btrfs";
-          extraArgs = ["-f"];
-          subvolumes = {
-            "/nix-store" = {
-              mountpoint = "/nix/store";
-              mountOptions = [
-                "compress=zstd"
-                "noatime"
-              ];
-            };
-            # When using impermance, use this as the "tmpfs" subvolume.
-            "/root" = {
-              mountpoint = "/";
-              mountOptions = [
-                "compress=zstd"
-                "noatime"
-              ];
-            };
-          };
-        };
-      };
-    };
-
-    swap = {
-      type = "8200";
-      size = "20G";
-      priority = "300";
-      content = {
-        type = "swap";
-        randomEncryption = true;
-      };
-    };
-
-    home = {
-      type = "8302";
-      size = "100%";
-      content = {
-        type = "filesystem";
-	format = "ext4";
-      };
-    };
-
-    rescue = {
-      type = "8301";
-      start = "-10G";
-      end = "-0";
-      content.type = "raw";
-    };
-  };
 in {
   disko.devices.disk.nixos = {
     type = "disk";
     device = "/dev/nvme0n1";
     content = {
       type = "gpt";
-      inherit partitions;
+
+      partitions = {
+        efiboot = {
+          size = "512M";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = ["umask=0077"];
+          };
+        };
+
+        # Nix store & impermanence old roots
+        system = {
+          size = "100G";
+          priority = "200";
+          content = {
+            type = "luks";
+            name = "nixos-system";
+            settings = {
+              allowDiscards = true;
+            };
+            content = {
+              type = "btrfs";
+              extraArgs = ["-f"];
+              subvolumes = {
+                "/nix-store" = {
+                  mountpoint = "/nix/store";
+                  mountOptions = [
+                    "compress=zstd"
+                    "noatime"
+                  ];
+                };
+                # When using impermance, use this as the "tmpfs" subvolume.
+                "/root" = {
+                  mountpoint = "/";
+                  mountOptions = [
+                    "compress=zstd"
+                    "noatime"
+                  ];
+                };
+              };
+            };
+          };
+        };
+
+        swap = {
+          size = "20G";
+          type = "8200";
+          priority = "300";
+          content = {
+            type = "swap";
+            randomEncryption = true;
+          };
+        };
+
+        home = {
+          type = "8302";
+          size = "100%";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+          };
+        };
+
+        rescue = {
+          type = "8301";
+          start = "-10G";
+          end = "-0";
+        };
+      };
     };
   };
 }
