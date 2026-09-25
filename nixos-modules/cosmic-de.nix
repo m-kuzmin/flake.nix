448da0d9b4d992587f218cce7e1766f8e52eb49b
m-kuzmin <71077087+m-kuzmin@users.noreply.github.com>
2026-09-26 01:00:06 +0300

    nixos: Add more sane defaults to the Cosmic DE module

--- nixos-modules/cosmic-de.nix
+++ nixos-modules/cosmic-de.nix
@@ -1,4 +1,15 @@
 {
   services.desktopManager.cosmic.enable = true;
   services.displayManager.cosmic-greeter.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    audio.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+  };
+
+  services.libinput.enable = true;
+  services.fprintd.enable = true;
 }
